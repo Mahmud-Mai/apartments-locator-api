fbe48703b1facdaf1f7f40faba9fbc29398d4330
Mahmud Mai <mahmudx00@gmail.com>
2023-09-28 11:12:37 +0100

    Create routes for each crud operation on apartments

--- routes/apartments.js
+++ routes/apartments.js
@@ -1,7 +1,19 @@
 import { Router } from "express";
-import { getAllApartments } from "../controllers/apartments.js";
+import {
+  getAllApartments,
+  createApartment,
+  getApartmentById,
+  updateApartment,
+  deleteApartment,
+} from "../controllers/apartments.js";
+import validateApartment from "../utils/validateApartment.js";
+
 const router = Router();
 
 router.route("/").get(getAllApartments);
+router.route("/").post(validateApartment, createApartment);
+router.route("/:id").get(getApartmentById);
+router.route("/:id").put(validateApartment, updateApartment);
+router.route("/:id").delete(deleteApartment);
 
 export default router;
