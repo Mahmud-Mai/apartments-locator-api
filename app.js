ac00d13e555989715f99bfe359db8cc3abd5db08
Mahmud Mai <mahmudx00@gmail.com>
2023-09-28 11:15:28 +0100

    Import connectDB function

--- app.js
+++ app.js
@@ -1,10 +1,12 @@
 import express, { json, urlencoded } from "express";
 import cookieParser from "cookie-parser";
 import logger from "morgan";
+import "dotenv/config";
 
 import indexRouter from "./routes/index.js";
 import usersRouter from "./routes/users.js";
 import apartmentsRouter from "./routes/apartments.js";
+import { connect } from "./utils/db.js";
 
 const app = express();
 
@@ -17,5 +19,7 @@ app.use(express.static("public"));
 app.use("/", indexRouter);
 app.use("/users", usersRouter);
 app.use("/apartments", apartmentsRouter);
+
+connect();
 
 export default app;
