94edc087b8cb621138ab2e463a379f1b239afaeb
Mahmud Mai <mahmudx00@gmail.com>
2023-10-03 11:08:15 +0100

    Create routes for user endpoint

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,18 @@
 import { Router } from "express";
+import {
+  getAllUsers,
+  createUser,
+  getUserById,
+  updateUser,
+  deleteUser,
+} from "../controllers/users.js";
+
 const router = Router();
 
-/* GET users listing. */
-router.get("/", function (req, res, next) {
-  res.send("respond with a resource");
-});
+router.route("/").get(getAllUsers);
+router.route("/").post(createUser);
+router.route("/:id").get(getUserById);
+router.route("/:id").put(updateUser);
+router.route("/:id").delete(deleteUser);
 
 export default router;
