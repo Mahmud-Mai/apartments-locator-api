e6d603ae750ce16fd0c3320752b27a20d9e5d835
Mahmud Mai <mahmudx00@gmail.com>
2023-10-03 09:47:05 +0100

    Modify apartments to reference room model

---
+++ models/apartment.js
@@ -0,0 +1,44 @@
+import { Schema, model } from "mongoose";
+
+const apartmentSchema = new Schema({
+  name: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  description: String,
+  address: {
+    type: String,
+    required: true,
+  },
+  neighbourhood: {
+    type: String,
+    enum: ["Zaria", "Samaru", "Dogon Itche", "Zango"],
+    required: true,
+  },
+  price: {
+    type: Number,
+    required: true,
+  },
+  images: [String],
+  mapsCoordinates: {
+    type: {
+      type: String,
+      enum: ["Point"],
+    },
+    coordinates: {
+      type: [Number],
+    },
+  },
+  rooms: [
+    {
+      type: Schema.Types.ObjectId,
+      ref: "Room",
+      required: true,
+    },
+  ],
+});
+
+const Apartment = model("Apartment", apartmentSchema);
+
+export default Apartment;
